ddaefb260d839c2ff49806b3ee880b89ecd5a263
hexiy <16806036+hexiy@users.noreply.github.com>
2024-02-15 15:57:05 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  // Redirect all traffic to "https://linktr.ee/austinai"
-  return NextResponse.redirect("https://linktr.ee/austinai");
+  // Redirect all traffic to "https://linktr.ee/hexiy"
+  return NextResponse.redirect("https://linktr.ee/hexiy");
 }
